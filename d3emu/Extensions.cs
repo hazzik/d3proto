f9ed9089a81c83828abf79d06c00068c5737d2b4
TOM_RUS <tom_rus@getmangos.com>
2011-09-13 19:11:52 +0400

    Fix.

---
+++ d3emu/Extensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace d3emu
+{
+    public static class Extenstions
+    {
+        public static uint ToUnixTime(this DateTime time)
+        {
+            return (uint)((time.ToUniversalTime().Ticks - 621355968000000000L) / 10000000L);
+        }
+
+        public static byte[] Append(this byte[] a, byte[] b)
+        {
+            var result = new byte[a.Length + b.Length];
+
+            a.CopyTo(result, 0);
+            b.CopyTo(result, a.Length);
+
+            return result;
+        }
+
+        public static byte[] ToByteArray(this string str)
+        {
+            str = str.Replace(" ", String.Empty);
+
+            var res = new byte[str.Length / 2];
+            for (int i = 0; i < res.Length; ++i)
+            {
+                string temp = String.Concat(str[i * 2], str[i * 2 + 1]);
+                res[i] = Convert.ToByte(temp, 16);
+            }
+            return res;
+        }
+
+        public static string ToHexDump(this byte[] byteArray)
+        {
+            var retStr = String.Empty;
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                if (i > 0 && ((i % 16) == 0))
+                    retStr += Environment.NewLine;
+                retStr += byteArray[i].ToString("X2") + " ";
+            }
+            retStr += Environment.NewLine;
+            return retStr;
+        }
+
+        public static string ToHexString(this byte[] byteArray)
+        {
+            string retStr = "";
+            foreach (byte b in byteArray)
+                retStr += b.ToString("x2");
+            return retStr;
+        }
+
+        public static bool CompareTo(this byte[] byteArray, byte[] second)
+        {
+            if (byteArray.Length != second.Length)
+                return false;
+
+            for (var i = 0; i < byteArray.Length; ++i)
+            {
+                if (second[i] != byteArray[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- d3emu/Program.cs
+++ d3emu/Program.cs
@@ -206,14 +206,16 @@ namespace d3emu
             // response.ModuleHandle.Region: US
             // response.ModuleHandle.Usage: auth
             // response.ModuleHandle.Hash: 8f52906a2c85b416a595702251570f96d3522f39237603115f2f1ab24962043c (SHA256 of auth module file)
-            // response.message: ... (SRP parameters?)
+            // response.message: ... (SRP parameters)
             // message format:
             // size for command 1 is 321 bytes
             //     byte command;
             //     if (command == 0 || command == 1)
             //     {
             //         if (command == 0)
+            //         {
             //             byte accountSalt[32];
+            //         }
             //         byte passwordSalt[32];
             //         byte serverChallenge[128];
             //         byte secondaryChallenge[128];
@@ -268,72 +270,6 @@ namespace d3emu
             var header = new byte[] { 0xFE, 0x03, 0x03, 0x00, 0x00 }; // wrong password response?
 
             Send(s, header);
-        }
-    }
-
-    public static class Extenstions
-    {
-        public static uint ToUnixTime(this DateTime time)
-        {
-            return (uint)((time.ToUniversalTime().Ticks - 621355968000000000L) / 10000000L);
-        }
-
-        public static byte[] Append(this byte[] a, byte[] b)
-        {
-            var result = new byte[a.Length + b.Length];
-
-            a.CopyTo(result, 0);
-            b.CopyTo(result, a.Length);
-
-            return result;
-        }
-
-        public static byte[] ToByteArray(this string str)
-        {
-            str = str.Replace(" ", String.Empty);
-
-            var res = new byte[str.Length / 2];
-            for (int i = 0; i < res.Length; ++i)
-            {
-                string temp = String.Concat(str[i * 2], str[i * 2 + 1]);
-                res[i] = Convert.ToByte(temp, 16);
-            }
-            return res;
-        }
-
-        public static string ToHexDump(this byte[] byteArray)
-        {
-            var retStr = String.Empty;
-            for (int i = 0; i < byteArray.Length; i++)
-            {
-                if (i > 0 && ((i % 16) == 0))
-                    retStr += Environment.NewLine;
-                retStr += byteArray[i].ToString("X2") + " ";
-            }
-            retStr += Environment.NewLine;
-            return retStr;
-        }
-
-        public static string ToHexString(this byte[] byteArray)
-        {
-            string retStr = "";
-            foreach (byte b in byteArray)
-                retStr += b.ToString("x2");
-            return retStr;
-        }
-
-        public static bool CompareTo(this byte[] byteArray, byte[] second)
-        {
-            if (byteArray.Length != second.Length)
-                return false;
-
-            for (var i = 0; i < byteArray.Length; ++i)
-            {
-                if (second[i] != byteArray[i])
-                    return false;
-            }
-
-            return true;
         }
     }
 }
