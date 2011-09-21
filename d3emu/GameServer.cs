2c89e3832ffe754447f0ddccdcfedd9e3b9904f1
TOM_RUS <tom_rus@getmangos.com>
2011-09-21 17:10:50 +0400

    Moved GS Socket to separate file.

---
+++ d3emu/GameServer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace d3emu
+{
+    class GameServer
+    {
+        private bool first = true;
+        private Socket m_gameSocket;
+        private NetworkStream m_gameStream;
+        private byte[] m_buffer = new byte[2048];
+
+        public GameServer()
+        {
+            m_gameSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            m_gameSocket.Bind(new IPEndPoint(IPAddress.Any, 6665));
+
+            m_gameSocket.Listen(10);
+
+            BeginAccept();
+        }
+
+        private void BeginAccept()
+        {
+            Console.WriteLine("GS: Waiting for connections...");
+            m_gameSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private void BeginRead()
+        {
+            m_gameStream.BeginRead(m_buffer, 0, m_buffer.Length, new AsyncCallback(ReadCallback), null);
+        }
+
+        private void ReadCallback(IAsyncResult result)
+        {
+            int received = 0;
+
+            try
+            {
+                received = m_gameStream.EndRead(result);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                return;
+            }
+
+            if (received > 0)
+            {
+                byte[] data = new byte[received];
+                Array.Copy(m_buffer, data, received);
+
+                Console.WriteLine("GS: Received packet, len = {0}", received);
+                data.PrintHex();
+
+                if (first) // send hardcoded packets after first client packet
+                {
+                    first = false;
+                    new HardcodedGsPackets(m_gameStream);
+                }
+
+                using (BinaryReader br = new BinaryReader(new MemoryStream(data)))
+                {
+                    var size = br.ReadInt32Reversed(); // includes size of this field as well
+
+                    var opcode = br.ReadUInt16(); // ?
+
+                    Console.WriteLine("GS: Opcode {0:X4}, size {1}", opcode, size);
+
+                    //if (opcode == 0x783C) // movement
+                    //{
+                    //    // can't seems figure this out yet
+
+                    //    var x1 = br.ReadByte();
+                    //    var y1 = br.ReadByte();
+                    //    var z1 = br.ReadByte();
+
+                    //    var x2 = br.ReadSingle();
+                    //    var y2 = br.ReadSingle();
+                    //    var z2 = br.ReadSingle();
+
+                    //    var x3 = br.ReadByte();
+                    //    var y3 = br.ReadByte();
+                    //    var z3 = br.ReadByte();
+
+                    //    var x4 = br.ReadSingle();
+                    //    var y4 = br.ReadSingle();
+
+                    //    Console.WriteLine("Move: {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10}", x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4);
+                    //}
+                }
+
+                BeginRead();
+            }
+            else
+            {
+                Console.WriteLine("GS: Disconnected!");
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult result)
+        {
+            Socket client = m_gameSocket.EndAccept(result);
+
+            m_gameStream = new NetworkStream(client);
+
+            Console.WriteLine("GS: Client connected...");
+
+            BeginRead();
+        }
+    }
+}
--- d3emu/Program.cs
+++ d3emu/Program.cs
@@ -4,14 +4,13 @@ using System.Net.Sockets;
 
 namespace d3emu
 {
-    using System.IO;
     using System.Threading;
 
     static class Program
     {
         private static Socket m_socket;
 
-        private static Socket m_gameSocket;
+        private static GameServer m_gameServer;
 
         public const byte PrevService = 0xFE;
 
@@ -23,97 +22,14 @@ namespace d3emu
 
             m_socket.Listen(10);
 
-            m_gameSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            m_gameSocket.Bind(new IPEndPoint(IPAddress.Any, 6665));
-
-            m_gameSocket.Listen(10);
-
-            BeginAccept();
+            m_gameServer = new GameServer();
 
             while (true)
             {
-                Console.WriteLine("Waiting for connections...");
+                Console.WriteLine("BN: Waiting for connections...");
                 var client = m_socket.Accept();
                 new Thread(() => new Client(client).Run()).Start();
             }
-        }
-
-        private static bool first = true;
-
-        private static NetworkStream m_gameStream;
-
-        private static byte[] m_buffer = new byte[2048];
-
-        private static void BeginAccept()
-        {
-            m_gameSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
-        }
-
-        private static void BeginRead()
-        {
-            m_gameStream.BeginRead(m_buffer, 0, m_buffer.Length, new AsyncCallback(ReadCallback), null);
-        }
-
-        private static void ReadCallback(IAsyncResult result)
-        {
-            int received = m_gameStream.EndRead(result);
-
-            if (received > 0)
-            {
-                byte[] data = new byte[received];
-                Array.Copy(m_buffer, data, received);
-
-                Console.WriteLine("Received GS Packet {0} bytes:", received);
-                data.PrintHex();
-
-                if (first) // send hardcoded packets after first client packet
-                {
-                    first = false;
-                    new HardcodedGsPackets(m_gameStream);
-                }
-
-                using (BinaryReader br = new BinaryReader(new MemoryStream(data)))
-                {
-                    var size = br.ReadInt32Reversed(); // includes size of this field as well
-
-                    var opcode = br.ReadUInt16(); // ?
-
-                    Console.WriteLine("Size {0}, opcode? {1:X4}", size, opcode);
-
-                    if (opcode == 0x783C) // movement
-                    {
-                        // can't seems figure this out yet
-                        //var unk4 = br.ReadUInt16();
-                        //var unk1 = br.ReadSingle();
-                        //var unk3 = br.ReadSingle();
-                        //var unk2 = br.ReadSingle();
-                        //var x = br.ReadSingle();
-                        //var y = br.ReadSingle();
-                        //var z = br.ReadSingle();
-                        //var o = br.ReadSingle();
-
-                        //Console.WriteLine("Move: {0} {1} {2} {3} {4} {5} {6} {7}", x, y, z, o, unk1, unk2, unk3, unk4);
-                    }
-                }
-
-                BeginRead();
-            }
-            else
-            {
-                Console.WriteLine("Disconnected!");
-            }
-        }
-
-        private static void AcceptCallback(IAsyncResult result)
-        {
-            Socket client = m_gameSocket.EndAccept(result);
-
-            m_gameStream = new NetworkStream(client);
-
-            Console.WriteLine("Client connected...");
-
-            BeginRead();
         }
     }
 }
